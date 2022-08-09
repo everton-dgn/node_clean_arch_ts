ed5e38713acca896ff54f0509c103c3e3e304881
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-09 02:18:08 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/add-account/__test__/db-add-account.test.ts
+++ src/data/usecases/add-account/__test__/db-add-account.test.ts
@@ -1,15 +1,26 @@
+import { Encrypter } from '@src/data/protocols/encrypter'
 import { DbAddAccount } from '@src/data/usecases'
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async encrypt(password: string): Promise<string> {
+      return Promise.resolve('hashed_password')
+    }
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return { sut, encrypterStub }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async encrypt(password: string): Promise<string> {
-        return Promise.resolve('hashed_password')
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
