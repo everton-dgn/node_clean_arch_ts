1da33fe81f25000c5a515922f9a547b1b2bbf42d
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-09 00:49:49 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -2,7 +2,6 @@ import { EmailValidator } from '@src/presentation/protocols/email-validator'
 import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isValid(email: string): boolean {
     return validator.isEmail(email)
   }
