df64c3060fca09cbb88ffdf862fb9c88a6bb0096
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-24 02:12:59 -0300

    test: ensure AccountMongoRepository returns account on success

---
+++ src/infra/db/mongodb/account-repository/__tests__/account.test.ts
@@ -0,0 +1,28 @@
+import { AccountMongoRepository } from '..'
+import { MongoHelper } from '@src/infra/db/mongodb/helpers'
+
+const url = process.env.MONGO_URL as string
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(url)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
