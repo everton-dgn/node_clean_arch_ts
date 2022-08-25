63776c5b1a7247bee4952ce74ff67cddbbece086
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-25 11:34:01 -0300

    refactor: simplifies code for data fetching

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,12 +4,10 @@ import { AddAccountModel } from '@src/domain/usecases'
 import { MongoHelper } from '../helpers'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-    const account = result.ops[0]
-    const { _id, ...accountWithoutId } = account
-    return Object.assign({}, accountWithoutId, { id: _id })
+    const account = { id: result.insertedId.toString(), ...accountData }
+    return account
   }
 }
